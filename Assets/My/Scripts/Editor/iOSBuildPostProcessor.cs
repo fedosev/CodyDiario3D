d19e31d3048d6730e491335dbb35b0cf3a4f5b5b
Andrey Fedoseev <fedosev@gmail.com>
2017-08-09 09:22:49 +0200

    Add icons and iOSBuildPostProcessor

---
+++ Assets/My/Scripts/Editor/iOSBuildPostProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+public class iOSBuildPostProcessor : MonoBehaviour {
+
+	#if UNITY_IOS
+		[PostProcessBuild]
+		static void OnPostprocessBuild(BuildTarget buildTarget, string path)
+		{
+			var plistPath = Path.Combine(path, "Info.plist");
+			var plist = new PlistDocument();
+			plist.ReadFromFile(plistPath);
+	
+			PlistElementDict rootDict = plist.root;
+			rootDict.SetString("NSCameraUsageDescription", "La camera viene usata per la realtà aumentata");
+	
+			File.WriteAllText(plistPath, plist.WriteToString());
+		}
+	#endif
+}
