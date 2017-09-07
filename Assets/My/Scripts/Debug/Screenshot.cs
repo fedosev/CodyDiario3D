d50e3ddbc54500b7235c31bc3f1dcc2fe86c2dc7
Andrey Fedoseev <fedosev@gmail.com>
2017-09-07 14:15:31 +0200

    Add reality scene for screenshots

---
+++ Assets/My/Scripts/Debug/Screenshot.cs
@@ -0,0 +1,18 @@
+﻿#if UNITY_EDITOR
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Screenshot : MonoBehaviour {
+
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)) {
+			ScreenCapture.CaptureScreenshot("screenshots/screen-" + System.DateTime.Now.Ticks + ".png", 2);
+		}
+	}
+}
+
+#endif
